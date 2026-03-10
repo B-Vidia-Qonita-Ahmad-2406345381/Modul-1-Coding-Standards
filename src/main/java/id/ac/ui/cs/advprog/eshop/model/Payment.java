5c3e5f2f5089fcd5330495e064a76575631c3ab4
vidiaqonita <qonitavidia@gmail.com>
2026-03-10 17:20:22 +0700

    [GREEN] Implement Payment model

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -13,13 +13,37 @@ import java.util.Map;
 public class Payment {
     String id;
     String method;
-    @Setter
     String status;
     Map<String, String> paymentData;
 
     public Payment(String id, String method, Map<String,String> paymentData) {
+        this.id = id;
+        this.method = method;
+        this.paymentData = paymentData;
+        this.status = "WAITING";
     }
     public Payment(String id, String method, Map<String,String> paymentData, String status) {
+        this.id = id;
+        this.method = method;
+        this.paymentData = paymentData;
+        String[] statusList = {"WAITING",
+                "SUCCESS",
+                "REJECTED"};
+        if (Arrays.stream(statusList).noneMatch(item -> (item.equals(status)))) {
+            throw new IllegalArgumentException();
+        } else {
+            this.status = status;
+        }
+    }
+    public void setStatus(String status) {
+        String[] statusList = {"WAITING",
+                "SUCCESS",
+                "REJECTED"};
+        if (Arrays.stream(statusList).noneMatch(item -> (item.equals(status)))) {
+            throw new IllegalArgumentException();
+        } else {
+            this.status = status;
+        }
     }
 }
 
