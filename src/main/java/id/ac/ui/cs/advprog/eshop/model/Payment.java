e235c2686c1c3f1bc7644240f501cc118474d8b3
vidiaqonita <qonitavidia@gmail.com>
2026-03-10 16:47:30 +0700

    [RED] Add Payment model skeleton + fix Payment model test

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -0,0 +1,25 @@
+package id.ac.ui.cs.advprog.eshop.model;
+
+import id.ac.ui.cs.advprog.eshop.enums.OrderStatus;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+@Getter
+public class Payment {
+    String id;
+    String method;
+    @Setter
+    String status;
+    Map<String, String> paymentData;
+
+    public Payment(String id, String method, Map<String,String> paymentData) {
+    }
+    public Payment(String id, String method, Map<String,String> paymentData, String status) {
+    }
+}
+
--- src/test/java/id/ac/ui/cs/advprog/eshop/model/PaymentTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/model/PaymentTest.java
@@ -1,4 +1,81 @@
 package id.ac.ui.cs.advprog.eshop.model;
 
-public class PaymentTest {
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PaymentTest {
+
+    private Map<String, String> paymentData;
+
+    @BeforeEach
+    void setUp() {
+        paymentData = new HashMap<>();
+        paymentData.put("voucherCode", "ESHOP1234ABC5678");
+    }
+
+    @Test
+    void testCreatePaymentDefaultStatus() {
+        Payment payment = new Payment(
+                "payment-1",
+                "VoucherCode",
+                paymentData
+        );
+
+        assertEquals("payment-1", payment.getId());
+        assertEquals("VoucherCode", payment.getMethod());
+        assertEquals(paymentData, payment.getPaymentData());
+    }
+
+    @Test
+    void testCreatePaymentWithStatus() {
+        Payment payment = new Payment(
+                "payment-1",
+                "VoucherCode",
+                paymentData,
+                "SUCCESS"
+        );
+
+        assertEquals("SUCCESS", payment.getStatus());
+    }
+
+    @Test
+    void testCreatePaymentInvalidStatus() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Payment(
+                    "payment-1",
+                    "VoucherCode",
+                    paymentData,
+                    "MEOW"
+            );
+        });
+    }
+
+    @Test
+    void testSetStatusSuccess() {
+        Payment payment = new Payment(
+                "payment-1",
+                "VoucherCode",
+                paymentData
+        );
+
+        payment.setStatus("SUCCESS");
+
+        assertEquals("SUCCESS", payment.getStatus());
+    }
+
+    @Test
+    void testSetStatusInvalidStatus() {
+        Payment payment = new Payment(
+                "payment-1",
+                "VoucherCode",
+                paymentData
+        );
+
+        assertThrows(IllegalArgumentException.class, () -> payment.setStatus("MEOW"));
+    }
 }
